b8a5049f2f806ffad7b18a20003de8f6f61f52ef
Maximiliano Usich <maximilianou@gmail.com>
2020-01-17 13:36:08 -0300

    [add] nodemon, realtime development docker-compose

--- src/notes/index.js
+++ src/notes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Notes!')
+  res.send(`Hello Notes! <br/>[${ (new Date).toISOString()}]`)
 })
 
 app.listen(3000)

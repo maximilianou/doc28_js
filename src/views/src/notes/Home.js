ae383d31c45469e523c440556e0501a78229578d
Maximiliano Usich <maximilianou@gmail.com>
2020-01-28 13:58:06 -0300

    [add] bridge docker-compose 172.23.0.1 ..

--- src/views/src/notes/Home.js
+++ src/views/src/notes/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
+//axios.defaults.baseURL = 'http://0.0.0.0';
+
 const Home = props => {
     useEffect(() => {
       axios.get('/api/hello')

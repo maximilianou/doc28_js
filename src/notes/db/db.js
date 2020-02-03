8f57d0126d2441aa7382cd59d4ed1c4b75b29f76
Maximiliano Usich <maximilianou@gmail.com>
2020-02-03 13:42:20 -0300

    [add] adding postgres, check now() api/db

--- src/notes/db/db.js
+++ src/notes/db/db.js
@@ -3,6 +3,9 @@ const pool = new Pool(
     {
         user: 'postgres',
         host: '172.23.0.1',
+        database: 'database1',
+        password: 'example',
+        port: 5432
         
     }
 );
--- src/notes/routes/index.js
+++ src/notes/routes/index.js
@@ -1,8 +1,25 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const pool = require('../db/db.js')
 
 router.get('/api/hello', (req, res) => {
 	res.json('hello world')
+})
+
+router.get('/api/db', (req, res) => {
+	;(async (req, res) => {
+		const client = await pool.connect()
+		try {
+		  const resSql = await client.query(' SELECT NOW() ');
+		  console.log(resSql.rows[0]);
+		  res.json(new String(resSql.rows[0]));
+		} finally {
+		  client.release();
+		}
+	  })(req, res).catch(err => {
+		  console.log(err.stack);
+		  res.json(err.stack);
+		})
 })
 
 module.exports = router
